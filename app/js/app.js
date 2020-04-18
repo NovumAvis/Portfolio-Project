3c6bdd31a9a9141db815d05b987d5ab73dcdaecb
Edward <novumavis@gmail.com>
2020-04-18 16:06:13 +0200

    First global update

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,35 @@
 document.addEventListener("DOMContentLoaded", function() {
-
-	// Custom JS
-
 });
+	// Custom JS
+$(document).ready(function() {
+	
+	// $(".cat-1").mouseenter(function(){
+	// 	$(".first-img").toggleClass("active");
+	// });
+
+
+
+	$(".cat-1").hover(function(){
+		$(".wall-1").toggleClass("show");
+		return false;
+	});
+
+	$(".cat-2").hover(function(){
+		$(".wall-2").toggleClass("show");
+		return false;
+	});
+
+	$(".cat-3").hover(function(){
+		$(".wall-3").toggleClass("show");
+		return false;
+	});
+
+	$(".cat-4").hover(function(){
+		$(".wall-4").toggleClass("show");
+		return false;
+	});
+
+
+
+}); //document ready
+
